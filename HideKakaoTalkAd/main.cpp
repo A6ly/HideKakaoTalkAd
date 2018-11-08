6ef8a7bbcb3922b489419f48043eaf218c4fdfc0
Turtledove <rlgus0626@gmail.com>
2018-11-09 01:19:29 +0900

    add files

---
+++ HideKakaoTalkAd/main.cpp
@@ -0,0 +1,20 @@
+#include <Windows.h>
+#include <stdio.h>
+
+int main() {
+
+	HWND KakaoMain, KakaoAd, KakaoChildWnd;
+	RECT Rect;
+
+	KakaoMain = FindWindow(NULL, TEXT("Ä«Ä«¿ÀÅå"));
+	KakaoAd = FindWindowEx(KakaoMain, NULL, "EVA_Window", NULL);
+	KakaoChildWnd = FindWindowEx(KakaoMain, NULL, "EVA_ChildWindow", NULL);
+
+	GetWindowRect(KakaoMain, &Rect);
+	SetWindowPos(KakaoAd, NULL, NULL, NULL, NULL, NULL, SWP_NOACTIVATE);
+	SetWindowPos(KakaoChildWnd, HWND_BOTTOM, NULL, NULL, (Rect.right - Rect.left - 2), (Rect.bottom - Rect.top - 33), SWP_NOMOVE);
+
+	printf("Hide KakaoTalk Ad!\n");
+
+	return 0;
+}
