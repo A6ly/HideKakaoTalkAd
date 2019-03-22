e6ed4aa44d0ee1bf69bf654a60ecf9fa49f8d24f
A6ly <a6ly26@gmail.com>
2019-03-22 23:51:07 +0900

    Commit

---
+++ HideKakaoTalkAd/HideKakaoTalkAd.cpp
@@ -0,0 +1,22 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+
+int main() {
+
+	HWND KakaoMain, KakaoAd, KakaoChildWnd;
+	RECT Rect;
+
+	KakaoMain = FindWindow(NULL, TEXT("카카오톡"));
+	KakaoAd = FindWindowEx(KakaoMain, NULL, "EVA_Window", NULL);
+	KakaoChildWnd = FindWindowEx(KakaoMain, NULL, "EVA_ChildWindow", NULL);
+
+	GetWindowRect(KakaoMain, &Rect);
+	SetWindowPos(KakaoAd, NULL, NULL, NULL, NULL, NULL, SWP_NOACTIVATE);
+	SetWindowPos(KakaoChildWnd, HWND_BOTTOM, NULL, NULL, (Rect.right - Rect.left - 2), (Rect.bottom - Rect.top - 33), SWP_NOMOVE);
+
+	printf("Hide KakaoTalk Ad!\n");
+	printf("If the ad doesn't get erased, please run it one more time^^7\n");
+	system("pause");
+
+	return 0;
+}
